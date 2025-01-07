83c8e2c9e3d21189b337874a754251b5f4922d43
danimohli <danimohlife@gmail.com>
2025-01-07 08:03:50 +0000

    betty std

--- executor.c
+++ executor.c
@@ -1,19 +1,19 @@
 #include "shell.h"
-
 /**
  * execute_command - Executes a command by searching for its path.
  * @command: The command to execute.
  */
 void execute_command(char *command)
 {
-    char *cmd_path = find_executable(command);
-    if (!cmd_path)
-    {
-        fprintf(stderr, "%s: command not found\n", command);
-        return;
-    }
-    fork_and_execute(cmd_path, command);
-    free(cmd_path);
+	char *cmd_path = find_executable(command);
+
+	if (!cmd_path)
+	{
+		fprintf(stderr, "%s: command not found\n", command);
+		return;
+	}
+	fork_and_execute(cmd_path, command);
+	free(cmd_path);
 }
 
 /**
@@ -23,23 +23,24 @@ void execute_command(char *command)
  */
 void fork_and_execute(char *cmd_path, char *command)
 {
-    pid_t pid = fork();
-    int status;
+	pid_t pid = fork();
+	int status;
 
-    if (pid == -1)
-    {
-        perror("fork");
-        return;
-    }
-    if (pid == 0)
-    {
-        char *argv[] = {command, NULL};
-        if (execve(cmd_path, argv, environ) == -1)
-            perror("execve");
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-        waitpid(pid, &status, 0);
-    }
+	if (pid == -1)
+	{
+		perror("fork");
+		return;
+	}
+	if (pid == 0)
+	{
+		char *argv[] = {command, NULL};
+
+		if (execve(cmd_path, argv, environ) == -1)
+			perror("execve");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+	}
 }
