0bb6b585625d373dd64d2c1b1ee0b407414c1145
danimohli <danimohlife@gmail.com>
2025-01-07 07:56:48 +0000

    command exc

--- executor.c
+++ executor.c
@@ -1,53 +1,45 @@
-#include "header.h"
+#include "shell.h"
 
-/* Function definitions */
+/**
+ * execute_command - Executes a command by searching for its path.
+ * @command: The command to execute.
+ */
 void execute_command(char *command)
 {
-	char *cmd_path = find_executable(command);
-	if (!cmd_path)
-	{
-		error_message(command);
-		return;
-	}
-	fork_and_execute(cmd_path, command);
-	free(cmd_path);
+    char *cmd_path = find_executable(command);
+    if (!cmd_path)
+    {
+        fprintf(stderr, "%s: command not found\n", command);
+        return;
+    }
+    fork_and_execute(cmd_path, command);
+    free(cmd_path);
 }
 
+/**
+ * fork_and_execute - Forks a process and executes the command.
+ * @cmd_path: The full path to the command.
+ * @command: The original command entered by the user.
+ */
 void fork_and_execute(char *cmd_path, char *command)
 {
-	pid_t pid = fork();
-	int status;
+    pid_t pid = fork();
+    int status;
 
-	if (pid == -1)
-	{
-		perror("fork");
-		return;
-	}
-	if (pid == 0)
-	{
-		char *argv[] = {command, NULL};
-		if (execve(cmd_path, argv, environ) == -1)
-			perror("execve");
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		waitpid(pid, &status, 0);
-	}
-}
-
-void build_command_path(char *cmd, char **cmd_path)
-{
-	*cmd_path = strdup(cmd);
-}
-
-void error_message(char *command)
-{
-	fprintf(stderr, "%s: command not found\n", command);
-}
-
-void free_resources(char *line, char *cmd_path)
-{
-	free(line);
-	free(cmd_path);
+    if (pid == -1)
+    {
+        perror("fork");
+        return;
+    }
+    if (pid == 0)
+    {
+        char *argv[] = {command, NULL};
+        if (execve(cmd_path, argv, environ) == -1)
+            perror("execve");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        waitpid(pid, &status, 0);
+    }
 }
